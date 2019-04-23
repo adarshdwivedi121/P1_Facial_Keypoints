00b7eb3bd5ca6f6be52721b79ce81e5dc5496685
Adarsh Dwivedi <adarshdwivedi121@gmail.com>
2019-04-23 11:03:30 +0530

    Project Completion
    Model successfully designed and implemented.
    Tested the model.

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -22,16 +21,38 @@ class Net(nn.Module):
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
         
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.norm2 = nn.BatchNorm2d(64)
+        
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.norm4 = nn.BatchNorm2d(256)
+        
+        self.drop = nn.Dropout(0.2)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.fc1 = nn.Linear(256*8*8, 4096)
+        self.fc2 = nn.Linear(4096, 768)
+        self.fc3 = nn.Linear(768, 136)
+        
+        
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
-
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.norm2(self.pool(F.relu(self.conv2(x))))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.norm4(self.pool(F.relu(self.conv4(x))))
         
+        x = x.view(x.size(0), -1)
+        
+        x = self.drop(F.relu(self.fc1(x)))
+        x = self.drop(F.relu(self.fc2(x)))
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
